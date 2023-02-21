0b61ab0f7183f2417fec2b93bd9b8e44a94280a0
Andrii_Petrov <Andrii_Petrov@epam.com>
2023-02-21 15:48:44 +0100

    check changes deployed automatically after push (no default deployment. Need to perform npm run deploy from the command line to deploy)

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload!!!
           .
         </p>
+        <p>Nice to see you here ))</p>
         <a
           className="App-link"
           href="https://reactjs.org"
