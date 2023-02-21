7988006d81f2157a24163a50bbc345e685d4b84d
Andrii_Petrov <Andrii_Petrov@epam.com>
2023-02-21 16:06:16 +0100

    npm install deploy works even for NOT commited/pushed changes (so local changes are used for creating build and deploying it to GitHub pages)

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!!!
-          .
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Alinka! Hi to you from Andrii from the React app hosted on GitHub Pages</p>
+        <p>Alinka!!! Hi to you from Andrii from the React app hosted on GitHub Pages</p>
         <p>Btw, I'm still waiting for you to scratch my back... ))</p>
         <a
           className="App-link"
